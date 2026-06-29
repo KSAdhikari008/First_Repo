af72e1bd4167a3597bb86f4f2bcaba2e2cccddca
Adhikari <adhikarifeb2001@gmail.com>
2026-06-29 20:08:53 +0530

    log sum of a b c d

---
+++ one.js
@@ -0,0 +1,9 @@
+conosle.log('karan');
+conosle.log('Singh');
+
+let a = 10;
+let b = 20;
+let c = 30;
+let d = 40;
+
+console.log(a+b+c+d);
---
+++ two.js
@@ -0,0 +1,6 @@
+const var1 = [1,2,3];
+const var2 = [4,5,6];
+
+const arr1 = var1.concat(var2);
+
+console.log(arr1);
